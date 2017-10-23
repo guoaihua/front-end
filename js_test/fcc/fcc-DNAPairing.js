9700b51ea0ca26f8b7ce1436c23476150f827723
guoaihua <308232015@qq.com>
2017-10-23 21:49:47 +0800

    This is a DNAPairing

---
+++ js_test/fcc/fcc-DNAPairing.js
@@ -0,0 +1,47 @@
+/*
+DNA 链缺少配对的碱基。依据每一个碱基，为其找到配对的碱基，然后将结果作为第二个数组返回。
+
+Base pairs（碱基对） 是一对 AT 和 CG，为给定的字母匹配缺失的碱基。
+
+在每一个数组中将给定的字母作为第一个碱基返回。
+
+例如，对于输入的 GCG，相应地返回 [["G", "C"], ["C","G"],["G", "C"]]
+
+字母和与之配对的字母在一个数组内，然后所有数组再被组织起来封装进一个数组。
+
+如果你被卡住了，记得开大招 Read-Search-Ask。尝试与他人结伴编程、编写你自己的代码。
+
+这是一些对你有帮助的资源:
+
+Array.push()
+String.split()
+*/
+
+/*
+* DNA 碱基配对  G-C A-T
+*/
+
+
+function pair(str) {
+  var arr = str.split("");
+  var arrNew = [];
+  arr.forEach(function(item,index,arr){
+      switch(item){
+        case "G":
+          arrNew.push(["G","C"]);
+          break;
+        case "C":
+          arrNew.push(["C","G"]);
+          break;
+        case "A":
+          arrNew.push(["A","T"]);
+          break;
+        case "T":
+          arrNew.push(["T","A"]);
+          break;
+      }
+  });
+  return arrNew;
+}
+
+console.log(pair("ATCGA"));
